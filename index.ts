397e2bb7c01134e9c0d0f7f86a2eb8235f227cb4
Weverson-Luan <wltech2022@gmail.com>
2023-09-12 01:12:43 -0300

    fix :bug: realizando correção na exportação do componmente inputws

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@
  * EXPORTS
  */
 export { TextNativeWs } from "./src/components/text/text";
-export { InputWs } from "./src/components/input/input";
+export { InputWs } from "./src/components/input";
 export { CheckBoxWs } from "./src/components/check-box/check-box";
 export { CreditCardWs } from "./src/components/credit-card/credit-card";
