d87ae3339eb9174d6932ce7a312d9b6a9c539b48
Weverson-Luan <wltech2022@gmail.com>
2023-09-16 17:56:58 -0300

    deploy :rocket: fazendo exportação do componente botão

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@
  */
 export { TextNativeWs } from "./src/components/text/text";
 export { InputWs } from "./src/components/input";
+export { ButtonWs } from "./src/components/button/button";
 export { CheckBoxWs } from "./src/components/check-box/check-box";
 export { CreditCardWs } from "./src/components/credit-card/credit-card";
